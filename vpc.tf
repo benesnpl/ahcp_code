a376ff495e8334afe8fcf3e31b71f70e5a4c9da2
Benelab <78307945+benesnpl@users.noreply.github.com>
2023-11-17 08:52:38 -0600

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,65 @@
+## Create VPCs
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block       					         = var.vpc_cidr
+  tags = {
+    Name                                     = var.vpc_name
+  }
+}
+
+resource "aws_vpc" "meraki_vpc" {
+  cidr_block       					         = var.vpc_cidr_vmx
+  tags = {
+    Name                                     = var.vpc_name_vmx
+  }
+}
+
+## Create Public Subnet
+
+resource "aws_subnet" "public_subnet" {
+    count                                   = length(var.public_cidr)
+    cidr_block                              = element(var.public_cidr,count.index)
+    vpc_id                                  = aws_vpc.main_vpc
+    availability_zone                       = element(var.azs,count.index)
+    map_public_ip_on_launch                 = false
+    tags = {
+        Name = ("Public-Subnet-AZ${count.index+1}")
+        }
+       }
+       
+resource "aws_subnet" "public_subnet_vmx" {
+    count                                   = length(var.public_cidr_vmx)
+    cidr_block                              = element(var.public_cidr_vmx,count.index)
+    vpc_id                                  = aws_vpc.meraki_vpc
+    availability_zone                       = var.az
+    map_public_ip_on_launch                 = false
+    tags = {
+        Name = ("Public-Subnet-VMx-AZ${count.index+1}")
+        }
+       }
+
+## Create Private Subnet
+
+
+resource "aws_subnet" "private_subnet" {
+    count                                   = length(var.private_cidr)
+    cidr_block                              = element(var.private_cidr,count.index)
+    vpc_id                                  = aws_vpc.main_vpc
+    availability_zone                       = element(var.azs,count.index)
+    map_public_ip_on_launch                 = false
+    tags = {
+        Name = ("Private-Subnet-AZ${count.index+1}")
+        }
+       }
+       
+
+resource "aws_subnet" "private_subnet_vmx" {
+    count                                   = length(var.private_cidr_vmx)
+    cidr_block                              = element(var.private_cidr_vmx,count.index)
+    vpc_id                                  = aws_vpc.meraki_vpc
+    availability_zone                       = var.az
+    map_public_ip_on_launch                 = false
+    tags = {
+        Name = ("Private-Subnet-VMx-AZ${count.index+1}")
+        }
+       }
